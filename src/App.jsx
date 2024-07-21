50cc0297b5e5f933bb5d635172ae7f11117e27b5
br-ndt <br.ndt.tyler@gmail.com>
2024-07-21 16:41:35 -0400

    add d20 physics and reroll onClick

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,59 @@
-import { Canvas, useLoader } from "@react-three/fiber";
-import { Physics, useSphere, usePlane } from "@react-three/cannon";
-import { Suspense } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { Physics, useConvexPolyhedron, usePlane } from "@react-three/cannon";
+import { Suspense, useCallback, useMemo, useState } from "react";
 import * as THREE from "three";
+
+import CannonUtils from "./CannonUtils";
+
 import "./App.css";
 
 const D20 = ({ position, color }) => {
-  const [ref] = useSphere(() => ({ 
-    mass: 1, 
+  const [hovered, setHover] = useState(false);
+  const geometry = useMemo(() => new THREE.IcosahedronGeometry(1, 0), []);
+  const args = useMemo(
+    () => CannonUtils.toConvexPolyhedronProps(geometry),
+    [geometry]
+  );
+  const [ref, api] = useConvexPolyhedron(() => ({
+    args,
+    mass: 1,
     position: position,
-    restitution: 0.8
+    restitution: 0.8,
+    rotation: resetRotation(),
   }));
+  const resetRotation = useCallback(() => {
+    return [
+      Math.random() * Math.PI,
+      Math.random() * Math.PI,
+      Math.random() * Math.PI,
+    ];
+  }, []);
+  const resetRoll = useCallback(() => {
+    setHover(false);
+    api.position.set(...position);
+    api.rotation.set(...resetRotation());
+  }, [api, resetRotation]);
+
+  useFrame(() => {
+    if (api.position.x > 6 || api.position.x < -6) {
+      resetRoll();
+    }
+  });
 
   return (
-    <mesh ref={ref} receiveShadow castShadow>
-      <icosahedronGeometry />
-      <meshStandardMaterial color={color} />
-    </mesh>
+    <>
+      <mesh
+        ref={ref}
+        receiveShadow
+        castShadow
+        onClick={(event) => resetRoll()}
+        onPointerOver={(event) => setHover(true)}
+        onPointerOut={(event) => setHover(false)}
+      >
+        <icosahedronGeometry />
+        <meshStandardMaterial color={hovered ? "yellow" : color} />
+      </mesh>
+    </>
   );
 };
 
@@ -23,7 +61,7 @@ const TablePlane = () => {
   const [ref] = usePlane(() => ({
     rotation: [-Math.PI / 2, 0, 0],
     position: [0, -3, 0],
-    restitution: 0.5
+    restitution: 0.5,
   }));
   const texture = useLoader(THREE.TextureLoader, "../assets/table.jpg");
 
@@ -47,7 +85,7 @@ const App = () => {
         </Suspense>
 
         <D20 position={[-2, 3, 0]} color={"red"} />
-        <D20 position={[2, 6, 0]} color={"green"} />
+        <D20 position={[2, 3, 0]} color={"green"} />
       </Physics>
     </Canvas>
   );
---
+++ src/CannonUtils.jsx
@@ -0,0 +1,82 @@
+// MIT License
+// Copyright (c) 2022, 2023, 2024 Sean Bradley
+// Based on https://sbcode.net/threejs/physics-cannonDebugrenderer/#srcclientutilscannonutilsts
+
+import * as THREE from "three";
+
+class CannonUtils {
+  static toConvexPolyhedronProps(geometry) {
+    const position = geometry.attributes.position;
+    const normal = geometry.attributes.normal;
+    const vertices = [];
+    for (let i = 0; i < position.count; i++) {
+      vertices.push(new THREE.Vector3().fromBufferAttribute(position, i));
+    }
+    const faces = [];
+    for (let i = 0; i < position.count; i += 3) {
+      const vertexNormals =
+        normal === undefined
+          ? []
+          : [
+              new THREE.Vector3().fromBufferAttribute(normal, i),
+              new THREE.Vector3().fromBufferAttribute(normal, i + 1),
+              new THREE.Vector3().fromBufferAttribute(normal, i + 2),
+            ];
+      const face = {
+        a: i,
+        b: i + 1,
+        c: i + 2,
+        normals: vertexNormals,
+      };
+      faces.push(face);
+    }
+
+    const verticesMap = {};
+    const points = [];
+    const changes = [];
+    for (let i = 0, il = vertices.length; i < il; i++) {
+      const v = vertices[i];
+      const key =
+        Math.round(v.x * 100) +
+        "_" +
+        Math.round(v.y * 100) +
+        "_" +
+        Math.round(v.z * 100);
+      if (verticesMap[key] === undefined) {
+        verticesMap[key] = i;
+        points.push({ x: vertices[i].x, y: vertices[i].y, z: vertices[i].z });
+        changes[i] = points.length - 1;
+      } else {
+        changes[i] = changes[verticesMap[key]];
+      }
+    }
+
+    const faceIdsToRemove = [];
+    for (let i = 0, il = faces.length; i < il; i++) {
+      const face = faces[i];
+      face.a = changes[face.a];
+      face.b = changes[face.b];
+      face.c = changes[face.c];
+      const indices = [face.a, face.b, face.c];
+      for (let n = 0; n < 3; n++) {
+        if (indices[n] === indices[(n + 1) % 3]) {
+          faceIdsToRemove.push(i);
+          break;
+        }
+      }
+    }
+
+    for (let i = faceIdsToRemove.length - 1; i >= 0; i--) {
+      const idx = faceIdsToRemove[i];
+      faces.splice(idx, 1);
+    }
+
+    const cannonFaces = faces.map(function (f) {
+      return [f.a, f.b, f.c];
+    });
+
+    return [points.map((v) => [v.x, v.y, v.z]), cannonFaces];
+  }
+}
+
+export default CannonUtils;
