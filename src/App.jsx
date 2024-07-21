9f0321e7aed66745d4e3156c5130240378285bee
Varun Nadgir <theminivann@gmail.com>
2024-07-21 12:12:10 -0400

    add physics
    add @react-three/cannon dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { Canvas, useLoader } from "@react-three/fiber";
+import { Physics, useSphere, usePlane } from "@react-three/cannon";
 import { Suspense } from "react";
 import * as THREE from "three";
 import "./App.css";
 
 const D20 = ({ position, color }) => {
+  const [ref] = useSphere(() => ({ 
+    mass: 1, 
+    position: position,
+    restitution: 0.8
+  }));
+
   return (
-    <mesh position={position}>
+    <mesh ref={ref} receiveShadow castShadow>
       <icosahedronGeometry />
       <meshStandardMaterial color={color} />
     </mesh>
@@ -13,11 +20,17 @@ const D20 = ({ position, color }) => {
 };
 
 const TablePlane = () => {
+  const [ref] = usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0],
+    position: [0, -3, 0],
+    restitution: 0.5
+  }));
   const texture = useLoader(THREE.TextureLoader, "../assets/table.jpg");
 
   return (
-    <mesh rotation={[-Math.PI / 4, 0, 0]} position={[0, -3, 0]}>
+    <mesh ref={ref} receiveShadow>
       <planeGeometry args={[50, 50]} />
+      <shadowMaterial color={"#171717"} transparent opacity={0.4} />
       <meshStandardMaterial map={texture} />
     </mesh>
   );
@@ -25,16 +38,17 @@ const TablePlane = () => {
 
 const App = () => {
   return (
-    <Canvas>
-      <directionalLight position={[0, 1, 1]} />
+    <Canvas shadows dpr={[1, 2]} gl={{ alpha: false }}>
+      <directionalLight position={[0, 3, 1]} castShadow />
 
-      <Suspense fallback={null}>
-        <TablePlane />
-      </Suspense>
+      <Physics>
+        <Suspense fallback={null}>
+          <TablePlane />
+        </Suspense>
 
-      <D20 position={[-2, 0, 0]} color={"red"} />
-      <D20 position={[2, 0, 0]} color={"green"} />
-      
+        <D20 position={[-2, 3, 0]} color={"red"} />
+        <D20 position={[2, 6, 0]} color={"green"} />
+      </Physics>
     </Canvas>
   );
 };
